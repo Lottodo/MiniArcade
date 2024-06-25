d908d69017903552595b01aca7451d97f2ecf18c
AxelJimenezHOLA <axel.jimenez85@uabc.edu.mx>
2024-06-24 18:41:56 -0700

    Agregar sonidos de Ace Attorney en SoundManager
    Se agregó código en el singleton de SoundManager que permite reproducir los sonidos del minijuego de Ace Attorney

--- singletons/SoundManager.gd
+++ singletons/SoundManager.gd
@@ -1,1 +1,20 @@
 extends Node
+
+const SOUND_ATTORNEY_OBJECTION = "attorney_objection"
+const SOUND_ATTORNEY_FAILURE = "attorney_failure"
+const SOUND_ATTORNEY_SELECT = "attorney_select"
+const SOUND_ATTORNEY_THINKING = "attorney_thinking"
+const SOUND_ATTORNEY_VICTORY = "attorney_victory"
+
+var SOUNDS = {
+	SOUND_ATTORNEY_OBJECTION : preload("res://assets/Attorney_Minigame/sounds/ace_attorney_objection.wav"),
+	SOUND_ATTORNEY_FAILURE : preload("res://assets/Attorney_Minigame/sounds/ace_attorney_failure.wav"),
+	SOUND_ATTORNEY_SELECT : preload("res://assets/Attorney_Minigame/sounds/ace_attorney_select.wav"),
+	SOUND_ATTORNEY_THINKING : preload("res://assets/Attorney_Minigame/sounds/ace_attorney_thinking.wav"),
+	SOUND_ATTORNEY_VICTORY: preload("res://assets/Attorney_Minigame/sounds/ace_attorney_victory.wav")
+}
+
+func play_sound(audio_player: AudioStreamPlayer2D, sound: String):
+	if SOUNDS.has(sound):
+		audio_player.stream = SOUNDS[sound]
+		audio_player.play()
