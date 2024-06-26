4b5fa71d4ac1cad48f9dd954408c828c82cf6c3e
Jlopez33 <jlopez33@uabc.edu.mx>
2024-06-26 12:54:00 -0700

    Corregir bug en minijuego Kirby
    Se corrige el bug de "segundo aire" en el minijuego de kirby y se añade el sprite sheet de la moneda

--- scenes/kirb_minigame/kirby_player/kirby_player.gd
+++ scenes/kirb_minigame/kirby_player/kirby_player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 var can_attack : bool = false
 var got_penalized : bool = false
 var already_attack : bool = false
+var got_hit : bool = false
 
 func _ready():
 	SignalManager.on_kirby_can_attack.connect(on_kirby_can_attack)
@@ -18,7 +19,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("action_A") and !can_attack:
 		got_penalized = true
 		SignalManager.on_kirby_got_penalized.emit()
-	elif Input.is_action_just_pressed("action_A") and !got_penalized and !already_attack:
+	elif Input.is_action_just_pressed("action_A") and !got_penalized and !already_attack and !got_hit:
 		SignalManager.on_player_reaction.emit()
 		animated_sprite2D.animation = "Attack"
 		position += Vector2(90,0)
@@ -31,6 +32,7 @@ func on_kirby_can_attack():
 	can_attack = true
 
 func on_kirby_hit():
+	got_hit = true
 	position += Vector2(90,0)
 	animated_sprite2D.animation = "Defeat"
 
