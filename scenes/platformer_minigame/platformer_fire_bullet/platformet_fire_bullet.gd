76beb33544c582e12dc3ab3df19d624409c84db7
Lottie <lottodo.dev@gmail.com>
2024-06-26 00:47:07 -0700

    retoques plataformas

--- scenes/platformer_minigame/platformer_fire_bullet/platformet_fire_bullet.gd
+++ scenes/platformer_minigame/platformer_fire_bullet/platformet_fire_bullet.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 @onready var sprite = $AnimatedSprite2D
+@onready var ending_timer = $EndingTimer
 
-var speed = -1000
+var speed = -1400
 var rot_speed: float = 3.0 * PI * 2.0
 var time: float
 
 func _ready():
+	var min_value = 1.0
+	var max_value = 1.5
+	#speed *= randf_range(min_value, max_value)
 	sprite.flip_h = true
 
 func _physics_process(delta):
@@ -27,5 +31,11 @@ func get_sin():
 
 
 func _on_screen_exited():
-	get_tree().call_group("Player", "on_proyectil_screen_exited")
+	ending_timer.start()
+	
 	#queue_free()
+
+
+
+func _on_ending_timer_timeout():
+	SignalManager.platformer_on_game_end.emit()
