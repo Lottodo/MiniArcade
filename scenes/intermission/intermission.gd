3f460e5366bf63c9507ff164110bb2c572453bf4
Lottie <lottodo.dev@gmail.com>
2024-06-26 20:15:07 -0700

    Score y vidas

--- scenes/intermission/intermission.gd
+++ scenes/intermission/intermission.gd
@@ -2,18 +2,37 @@ extends Control
 
 @onready var aspt = $AudioStreamPlayerTrans
 @onready var asps = $AudioStreamPlayerState
+@onready var camera = $Camera2D
+@onready var coins_container = $CoinsContainer
+@onready var score_label = $ScoreLabel
 var minigame_id = 0
+
+var coins: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	coins = coins_container.get_children()
+	score_label.text = str(GameManager.global_score)
+	
 	if GameManager.global_first_last_game_id == -1:
 		SoundManager.play_sound(aspt, SoundManager.SOUND_INTER_TRANS)
-	elif GameManager.global_last_game_won:
+	else:
+		update_game_state()
+	
+	select_minigame()
+
+func update_game_state():
+	update_coins()
+	if GameManager.global_last_game_won:
 		SoundManager.play_sound(asps, SoundManager.SOUND_INTER_WIN)
 	else:
 		SoundManager.play_sound(asps, SoundManager.SOUND_INTER_LOST)
-	select_minigame()
-	
+
+func update_coins():
+	var coins_not_visible = []
+	for coin_idx in range(coins.size()):
+		if coin_idx >= GameManager.global_lives:
+			coins[coin_idx].play("coin_destruction")
 
 func select_minigame():
 	var min_value = 0
